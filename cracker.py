cca34145415f36c9d4c1918b5d7f463ef12bf0bb
adrianroblescamporro <adrianrobles2706@gmail.com>
2023-02-16 10:31:18 +0100

    Programación de formcrack para obtener el login form

--- cracker.py
+++ cracker.py
@@ -1,9 +1,15 @@
 import requests
+import socket
 from requests.auth import HTTPBasicAuth, HTTPDigestAuth
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from html import HTML
+
+
 class Cracker():
     def __init__(self, url):
         self.numrequests = 0
         self.urlattack = url
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
     def basicauthcrack(self, username, password):
         response = requests.get(self.urlattack, auth=HTTPBasicAuth(username, password))
@@ -11,10 +17,25 @@ class Cracker():
             print('Usuario y contraseña válidos')
 
     def digestauthcrack(self, username, password):
-        response=requests.get(self.urlattack, auth=HTTPDigestAuth(username, password))
+        response = requests.get(self.urlattack, auth=HTTPDigestAuth(username, password))
         if response.status_code == 200:
             print('Usuario y contraseña válidos')
 
     def formcrack(self, username, password):
-        response=requests.get(self.urlattack)
-        print(response.text)
+        result = {'status': 0}
+        try:
+            response = requests.get(self.urlattack)
+        except requests.exceptions.ConnectionError:
+            result = {'status': -1}
+        except socket.timeout:
+            result = {'status': -2}
+
+        if result['status'] < 0:
+            print('Error in connection')
+
+        if response.status_code != 200 or response.status_code != 403:
+            print('Bad Status code')
+
+        htmlcode = HTML(response.text)
+        login_form = htmlcode.get_login_forms()
+        print(login_form)
